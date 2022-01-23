993a1860812a46bf2aadbcad91139af1ad136973
mandrykk <86601388+mandrykk@users.noreply.github.com>
2022-01-23 18:36:44 +0200

    phonebook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { v4 as uuidv4 } from 'uuid';
+import Phonebook from "./components/Phonebook/Phonebook";
+import Filter from "./components/Filter/Filter";
+import Contacts from "./components/Contacts/Contacts";
+import Container from "./components/Container/Container";
+  
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  onAddContact = (name, number) => {
+    if (this.onCheckContact(name)) {
+      alert(`${name} is already in contacts.`)
+      return
+    }
+    const obj = { id: uuidv4(), name, number }
+    this.setState((prevState) => ({ contacts: [...prevState.contacts, obj] }))
+  }
+
+  onCheckContact = (value) => {
+    return this.state.contacts.find(
+      (el) => el.name.toUpperCase() === value.toUpperCase(),
+    )
+  }
+
+  onDeleteContacts = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((el, index) => el.id !== id),
+    }))
+  }
+
+  onFiltering = (value) => {
+    this.setState({ filter: value })
+  }
+
+  render() {
+    const { contacts, filter } = this.state
+    return (
+      <div className="App">
+        <Container title="Phonebook">
+          <Phonebook onAddContact={this.onAddContact} />
+        </Container>
+        <Container title="Contacts">
+          {contacts.length >= 2 && (
+            <Filter filter={filter} onFilter={this.onFiltering} />
+          )}
+          <Contacts
+            listContacts={contacts.filter((el) =>
+              el.name.toUpperCase().includes(filter.toUpperCase()),
+            )}
+            onDelete={this.onDeleteContacts}
+          />
+        </Container>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
